3805f6f240e87207f763b2345c0921896afd96dc
robot <robot@207-pro3-3>
2022-09-20 09:28:27 +0900

    day1

---
+++ src/java_layoutExam/HelloButteon2.java
@@ -0,0 +1,31 @@
+package java_layoutExam;
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HelloButteon2 extends JFrame{
+	public HelloButteon2() {
+		JPanel p = new JPanel();
+		p.setBackground(Color.blue);
+		add(p);
+		
+		
+		//변경점 저장2
+		JButton btn = new JButton("버튼");
+		p.add(btn);
+		JButton btn1 = new JButton("버튼");
+		p.add(btn1);
+		
+		setTitle("안녕 버튼");
+		setSize(400, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		//1. new 생성자()로 윈도우 프로그램 실행 시키기
+		new HelloButteon2();
+	}
+}
